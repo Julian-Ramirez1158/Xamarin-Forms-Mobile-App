696e84c33dfe4701d956d79d7afb247ec3d70edb
Julian Ramirez <jram417@wgu.edu>
2022-11-23 00:25:04 -0500

    Add update term page and logic

---
+++ C971/UpdateTermPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C971.Models;
+using SQLite;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace C971
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UpdateTermPage : ContentPage
+    {
+        TermDetails TermDetails;
+        public UpdateTermPage(TermDetails termDetails)
+        {
+            InitializeComponent();
+            TermDetails = termDetails;
+            termTitle.Text = termDetails.selectedTerm.Title;
+            startDateEntered.Date = termDetails.selectedTerm.Start;
+            endDateEntered.Date = termDetails.selectedTerm.End;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            TermDetails.selectedTerm.Title = termTitle.Text;
+            TermDetails.selectedTerm.Start = startDateEntered.Date;
+            TermDetails.selectedTerm.End = endDateEntered.Date;
+            
+
+            SQLiteConnection connection = new SQLiteConnection(App.DatabaseLocation);
+
+            connection.CreateTable<Term>();
+
+            // update data
+            int rowsInserted = connection.Update(TermDetails.selectedTerm);
+
+            // close the connection
+            connection.Close();
+
+            if (rowsInserted > 0)
+            {
+                DisplayAlert("Success!", "Term succesffuly updated", "Close");
+                // TODO fix navigation problem where back button takes you to previously unsaved entry
+                
+                Navigation.PushAsync(new TermDetails(TermDetails.selectedTerm));
+            }
+            else
+            {
+                DisplayAlert("Failure!", "Term not updated", "Close");
+            }
+        }
+    }
+}
