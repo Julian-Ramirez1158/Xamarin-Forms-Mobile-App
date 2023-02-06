a3ffa2fbc0049a06f759cad6ec57b79d30b322ff
Julian Ramirez <jram417@wgu.edu>
2023-02-06 17:14:43 -0500

    Integrate sqlite queries to student reports entry properties for dynamic data

--- C971/CourseReport.xaml.cs
+++ C971/CourseReport.xaml.cs
@@ -15,33 +15,81 @@ namespace C971
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CourseReport : ContentPage
     {
+        public static int InProgress()
+        {
+            SQLiteConnection connection = new SQLiteConnection(App.DatabaseLocation);
+            // Creates table if one doesn't already exists
+            connection.CreateTable<Course>();
+            // Allows us to return the table query and turn it into a list
+            int inProgressCount = connection.Query<Course>("SELECT * FROM Course WHERE CourseStatus = 'In Progress'").Count();
+
+            connection.Close();
+            return inProgressCount;
+        }
+
+        public static int Dropped()
+        {
+            SQLiteConnection connection = new SQLiteConnection(App.DatabaseLocation);
+            // Creates table if one doesn't already exists
+            connection.CreateTable<Course>();
+            // Allows us to return the table query and turn it into a list
+            int droppedCount = connection.Query<Course>("SELECT * FROM Course WHERE CourseStatus = 'Dropped'").Count();
+
+            connection.Close();
+            return droppedCount;
+        }
+
+        public static int Completed()
+        {
+            SQLiteConnection connection = new SQLiteConnection(App.DatabaseLocation);
+            // Creates table if one doesn't already exists
+            connection.CreateTable<Course>();
+            // Allows us to return the table query and turn it into a list
+            int completedCount = connection.Query<Course>("SELECT * FROM Course WHERE CourseStatus = 'Completed'").Count();
+
+            connection.Close();
+            return completedCount;
+        }
+
+        public static int PlanToTake()
+        {
+            SQLiteConnection connection = new SQLiteConnection(App.DatabaseLocation);
+            // Creates table if one doesn't already exists
+            connection.CreateTable<Course>();
+            // Allows us to return the table query and turn it into a list
+            int pttCount = connection.Query<Course>("SELECT * FROM Course WHERE CourseStatus = 'Plan to take'").Count();
+
+            connection.Close();
+            return pttCount;
+        }
+
         private readonly ChartEntry[] entries = new[]
         {
-            new ChartEntry(1)
+            new ChartEntry(Dropped())
             {
                 Label = "Dropped",
-                ValueLabel = "1",
+                ValueLabel = $"{Dropped()}",
                 ValueLabelColor = SKColor.Parse("#2c3e50"),
                 Color = SKColor.Parse("#2c3e50")
             },
-            new ChartEntry(3)
+            new ChartEntry(InProgress())
             {
                 Label = "In Progress",
-                ValueLabel = "3",
+                ValueLabel = $"{InProgress()}",
                 ValueLabelColor = SKColor.Parse("#77d065"),
                 Color = SKColor.Parse("#77d065")
             },
-            new ChartEntry(12)
+            new ChartEntry(Completed())
             {
                 Label = "Completed",
-                ValueLabel = "12",
+                ValueLabel = $"{Completed()}",
                 ValueLabelColor = SKColor.Parse("#b455b6"),
                 Color = SKColor.Parse("#b455b6")
             },
-            new ChartEntry(8)
+            new ChartEntry(PlanToTake())
             {
                 Label = "Plan to take",
-                ValueLabel = "8",
+                ValueLabel = $"{PlanToTake()}",
                 ValueLabelColor = SKColor.Parse("#3498db"),
                 Color = SKColor.Parse("#3498db")
             }
@@ -49,7 +97,7 @@ namespace C971
         public CourseReport()
         {
             InitializeComponent();
-            chartViewPie.Chart = new PieChart { Entries = entries, LabelTextSize = 35 };
+            chartViewPie.Chart = new PieChart { Entries = entries, LabelTextSize = 25 };
 
         }
 
